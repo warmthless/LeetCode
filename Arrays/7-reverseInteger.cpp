587cf25533ecb4c8956c0734abc965e12b0ec74d
warmtheless <375511673@qq.com>
2018-07-10 21:57:36 +0800

    Create 7-reverseInteger.cpp

---
+++ Arrays/7-reverseInteger.cpp
@@ -0,0 +1,14 @@
+//12ms
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            if (abs(res) > INT_MAX / 10) return 0;
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        //if(x > INT_MAX) return 0;
+        return res;
+    }
+};
